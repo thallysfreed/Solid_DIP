b13cef9aa881e593cd041d61d2370a0b9740e9e4
Thallys Silva <thallysfreed@gmail.com>
2021-01-21 23:05:44 -0300

    Maneira errada

---
+++ src/com/silth/Main.java
@@ -0,0 +1,11 @@
+package com.silth;
+
+import com.silth.payment.Payment;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Payment payment = new Payment();
+        payment.pay("Bolo");
+    }
+}
